ff85b8523c464c834a11a3f1204b6b9190d35ac3
Israel <israel.cauich@campeche.gob.mx>
2022-08-29 15:32:26 -0500

    Inicial

---
+++ Aplicacion.Nucleo/IAplicacion.cs
@@ -0,0 +1,40 @@
+﻿using Infraestructura.Transversal.Autenticacion;
+using Infraestructura.Transversal.Seguimiento;
+using Serilog.Events;
+using Microsoft.Extensions.Configuration;
+
+namespace Aplicacion.Nucleo
+{
+    public interface IAplicacion : Dominio.Nucleo.IAplicacion
+    {
+        bool BasesDeDatosEnMemoria { get; }
+        /// <summary>
+        /// Obtiene la intancia de un logger.
+        /// </summary>
+        IApplicationLogger GetLogger();
+
+        /// <summary>
+        /// Obtiene la intancia de un token manager.
+        /// </summary>
+        ITokenManager GetTokenManager();
+
+        IConfiguration GetConfiguration();
+
+        /// <summary>
+        /// Permite cambiar el nivel minimo del log.
+        /// </summary>
+        void SwitchLogging(LogEventLevel level);
+
+        /// <summary>
+        /// Configura las unidades de trabajo para trabajar en memoria
+        /// </summary>
+        void UsarBasesDeDatosEnMemoria(string dbName = null);
+
+        /// <summary>
+        /// Configura las unidades de trabajo para trabajar con SQL
+        /// </summary>
+        void UsarBasesDeDatosSQL(string conexion = null);
+
+        void Authentication(string authority, string clientId, string clientSecret, bool requireHttps = true);
+    }
+}
